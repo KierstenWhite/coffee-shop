fc73d2c6cffb2cbd10b20cf296d920658d266730
Kiersten White <kierstenswhite@gmail.com>
2023-07-06 16:23:22 -0400

    Chapter 2, Exercise 3

--- js-app/main.js
+++ js-app/main.js
@@ -1,11 +1,20 @@
 const url = "https://localhost:5001/api/beanvariety/";
 
-
 const button = document.querySelector("#run-button");
+const beanList = document.querySelector("#beanList");
+
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            console.log(beanVarieties);
+            // console.log(beanVarieties);
+            let html = "<ul>"
+
+            beanVarieties.forEach(variety => {
+                html += `<li value=${variety.Id}>${variety.name}: From ${variety.region} | ${variety.notes}</li>`
+            });
+
+            html += "</ul>"
+            beanList.innerHTML = html
         })
 });
 
